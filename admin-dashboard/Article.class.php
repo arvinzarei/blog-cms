27842460c661af9b4ae03bbef650d005bb8e662c
arvin zarei <arvinzarei123@gmail.com>
2021-03-11 23:25:25 +0330

    Create article views and functions

--- admin-dashboard/Article.class.php
+++ admin-dashboard/Article.class.php
@@ -1,41 +1,43 @@
 <?php
+
 namespace AdminDashboard;
-require_once ("Admin.class.php");
-require_once (realpath(dirname(__FILE__) . "/DataBase.php"));
+require_once("Admin.class.php");
+require_once(realpath(dirname(__FILE__) . "/DataBase.php"));
+
 use DataBase\DataBase;
 
-class Article extends Admin{
+class Article extends Admin
+{
 
     public function index()
     {
-        $db=new DataBase();
-        $articles=$db->select("SELECT * FROM `articles` ORDER BY `id` DESC ");
-        require_once (realpath(dirname(__FILE__). "/../template/admin/articles/index.php"));
+        $db = new DataBase();
+        $articles = $db->select("SELECT * FROM `articles` ORDER BY `id` DESC ");
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/index.php"));
     }
 
     public function show($id)
     {
-        $db= new DataBase();
-        $article = $db->select("SELECT * FROM `articles` WHERE (`id` = ?); ",[$id])->fetch();
-        require_once (realpath(dirname(__FILE__). "/../template/admin/articles/show.php"));
+        $db = new DataBase();
+        $article = $db->select("SELECT * FROM `articles` WHERE (`id` = ?); ", [$id])->fetch();
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/show.php"));
     }
 
     public function create()
     {
-        $db= new DataBase();
+        $db = new DataBase();
         $categories = $db->select('SELECT * FROM `categories` ORDER BY `id` DESC ;');
-        require_once (realpath(dirname(__FILE__). "/../template/admin/articles/create.php"));
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/create.php"));
     }
 
     public function store($request)
     {
-        $db= new DataBase();
-        if ($request['cat_id'] !=null) {
-            $request['image']=$this->saveImage($request['image'],'article-image');
-            if ($request['image'])
-            {
-                $request=array_merge($request,array('user_id'=>1));
-                $db->insert('articles',array_keys($request), $request);
+        $db = new DataBase();
+        if ($request['cat_id'] != null) {
+            $request['image'] = $this->saveImage($request['image'], 'article-image');
+            if ($request['image']) {
+                $request = array_merge($request, array('user_id' => 1));
+                $db->insert('articles', array_keys($request), $request);
                 $this->redirect('article');
             } else
                 $this->redirectBack();
@@ -46,17 +48,42 @@ class Article extends Admin{
 
     public function edit($id)
     {
-
+        $db = new DataBase();
+        $article = $db->select("SELECT * FROM `articles` WHERE (`id` = ?); ", [$id])->fetch();
+        $categories = $db->select('SELECT * FROM `categories` ORDER BY `id` DESC ;');
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/edit.php"));
     }
 
-    public function update($Request,$id)
+    public function update($request, $id)
     {
-
+        $db = new DataBase();
+        if ($request['cat_id'] != null) {
+            $request['image'] = $this->saveImage($request['image'], 'article-image');
+            if ($request['image']) {
+                $request = array_merge($request, array('user_id' => 1));
+                $db->insert('articles', array_keys($request), $request);
+                $this->redirect('article');
+            } else {
+                unset($request['image']);
+            }
+            $request = array_merge($request, array('user_id' => 1));
+            $db->update('articles', $id, array_keys($request), $request);
+            $this->redirect('article');
+        }
+        else {
+                $this->redirectBack();
+            }
     }
 
     public function delete($id)
     {
-
+        //1 : remove Image
+        $db= new DataBase();
+        $article = $db->select("SELECT * FROM `articles` WHERE (`id` = ?); ", [$id])->fetch();
+        $this->removeImage($article['image']);
+        //2 : remove article
+        $db->delete('articles',$id);
+        $this->redirectBack();
     }
 
 }
