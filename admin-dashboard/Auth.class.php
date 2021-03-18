feaac8a2d135ab5333c07adde6e8d5ee1155846d
arvin zarei <arvinzarei123@gmail.com>
2021-03-18 16:43:05 +0330

    add Auth Class

---
+++ admin-dashboard/Auth.class.php
@@ -0,0 +1,56 @@
+<?php
+namespace AdminDashboard;
+require_once (realpath(dirname(__FILE__) . "/DataBase.php"));
+use DataBase\DataBase;
+
+class Auth{
+
+    public function login()
+    {
+        require_once (realpath(dirname(__FILE__). "/../template/auth/login.php"));
+    }
+
+    public function checkLogin($request)
+    {
+        if(empty($request['email']) || empty($request['password'])){
+            $this->redirectBack();
+        }
+        else if(strlen($request['password'] < 8) ){
+            $this->redirectBack();
+        }
+        else if(! filter_var($request['email'], FILTER_VALIDATE_EMAIL)){
+            $this->redirectBack();
+        }
+        else{
+            $db=new DataBase();
+            $user = $db->select("SELECT * FROM `users` WHERE (`email`= ?);",[$request['email']])->fetch();
+
+            if($user != null){
+                if(password_verify($request['password'],$user['password'])){
+                $_SESSION['user']=$user['id']; 
+                $this->redirect('admin'); 
+                }else{
+                    $this->redirectBack();
+                }
+            }
+            else{
+               $this->redirectBack(); 
+            }
+        } 
+    }
+
+    public function register()
+    {
+        require_once (realpath(dirname(__FILE__). "/../template/auth/register.php"));
+    }
+
+    protected function redirect($url){
+        $prtocol= stripos($_SERVER['SERVER_PROTOCOL'],'https')=== true ? 'https://' : 'http://';
+        header("Location: ".$prtocol.$_SERVER['HTTP_HOST']."/admin-panel/".$url);
+    }
+
+    protected function redirectBack(){
+        header("Location: ".$_SERVER['HTTP_REFERER']);
+    }
+
+}
