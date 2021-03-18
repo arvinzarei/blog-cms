70d35db559208ae6ceee938fc6f0ac89ab5c3fa6
arvin zarei <arvinzarei123@gmail.com>
2021-03-18 14:21:00 +0330

    some change in websetting file

--- admin-dashboard/WebSetting.class.php
+++ admin-dashboard/WebSetting.class.php
@@ -1,53 +1,42 @@
 <?php
 namespace AdminDashboard;
 require_once ("Admin.class.php");
-require_once (realpath(dirname(__FILE__) . "/DataBase.php"));
-use DataBase\DataBase;
+ require_once (realpath(dirname(__FILE__) . "/DataBase.php"));
+ use DataBase\DataBase;
+ class WebSetting extends Admin {
 
-class WebSetting extends Admin{
-
-    public function index()
-    {
+    public function index(){
         $db= new DataBase();
-        $setting = $db->select("SELECT * FROM `websetting`;" )->fetch();
+        $setting= $db->select("SELECT * FROM `websetting`;")->fetch();
         require_once (realpath(dirname(__FILE__). "/../template/admin/web-setting/index.php"));
     }
-
     public function set()
     {
         $db= new DataBase();
-        $setting = $db->select("SELECT * FROM `websetting`;" )->fetch();
+        $setting= $db->select("SELECT * FROM `websetting`;")->fetch();
         require_once (realpath(dirname(__FILE__). "/../template/admin/web-setting/set.php"));
     }
-
     public function store($request)
     {
-        $db=new DataBase();
+        $db= new DataBase();
+        $setting= $db->select("SELECT * FROM `websetting`;")->fetch();
         if($request['logo']['tmp_name'] != ""){
-            $request['logo']=$this->saveImage($request['logo'],'settin','logo');
+            $request['logo']= $this->saveImage($request['logo'],'setting','logo');
         }
-        else {
+        else{
             unset($request['logo']);
         }
         if($request['icon']['tmp_name'] != ""){
-            $request['icon']=$this->saveImage($request['icon'],'settin','icon');
+            $request['icon']= $this->saveImage($request['icon'],'setting','icon');
         }
-        else {
+        else{
             unset($request['icon']);
         }
-        if ($setting !=null) {
-            $db->update('websetting',$setting['id'],array_keys($request),$request);
-        }else {
-            $db->insert('websetting',array_keys($request),$request);
-        }
+        if($setting !=null)
+        $db->update('websetting', $setting['id'],array_keys($request),$request);
+        else
+        $db->insert('websetting',array_keys($request),$request);
         $this->redirect('web-setting');
-
-
+        
     }
-
-
-
-
-
-
-}
+ }
