5c3be0585a0713e9e3f7ca14df912633e2d2dbab
arvin zarei <arvinzarei123@gmail.com>
2021-03-20 13:21:06 +0330

    Complete sidebar section of page

--- template/app/layouts/sidebar.php
+++ template/app/layouts/sidebar.php
@@ -1,26 +1,28 @@
 <aside class="sidebar">
     <section class="sidebar-item">
         <ul class="sidebar-list">
-            <li><a href="http://localhost/admin-panel/show-category/1"><b>Lorem ipsum dolor.</b></a></li>
+        <?php foreach ($categories as $category) {?>
+            <li><a href="http://localhost/admin-panel/show-category/<?php echo $category['id']; ?>"><b><?php echo $category['name']; ?></b></a></li>
+        <?php } ?>
         </ul>
     </section>
     <section class="sidebar-item">
         <h2 class="title">POPULAR POSTS</h2>
-
+<?php foreach ($sidebarPopularArticles as $article) {?>
             <section class="popular-post">
-                <img class="popular-post-img" src="http://localhost/admin-panel/4" alt="">
+                <img class="popular-post-img" src="http://localhost/admin-panel/<?php echo $article['image']; ?>" alt="">
                 <section class="popular-post-title">
                     <h3>
-                        <a href="http://localhost/admin-panel/show-article/5"><b>Lorem ipsum dolor.</b></a>
+                        <a href="http://localhost/admin-panel/show-article/<?php echo $article['id']; ?>"><b><?php echo $article['title']; ?></b></a>
                     </h3>
                     <ul class="info-bar">
-                        <li class=""><span class="text-muted">by</span> <a href="#" class="color-black"><b>Kamran,</b></a>
-                            <span class="text-muted">feb 4 2019</span></li>
+                        <li class=""><span class="text-muted">by</span> <a href="#" class="color-black"><b><?php echo $article['username']; ?></b></a>
+                            <span class="text-muted"><?php echo date("M d,Y",strtotime( $article['created_at'])); ?></span></li>
                     </ul>
                 </section>
                 <section class="clear-fix"></section>
             </section>
+            <?php } ?>
     </section>
-
 
 </aside><!--end of sidebar-->
