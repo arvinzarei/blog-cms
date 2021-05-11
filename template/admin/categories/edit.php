79d77d382a06c0950399434224e5b12230513081
arvin zarei <arvinzarei123@gmail.com>
2021-05-11 20:54:03 +0430

    Update edit in categories

--- template/admin/categories/edit.php
+++ template/admin/categories/edit.php
@@ -1,9 +1,9 @@
 <?php
 require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
 ?>
-    <section class="pt-3 pb-1 mb-2 border-bottom">
-        <h1 class="h5">ویرایش دسته بندی</h1>
-    </section>
+<section class="pt-3 pb-1 mb-2 border-bottom">
+    <h1 class="h5">ویرایش دسته بندی</h1>
+</section>
 
 <section class="row my-3">
     <section class="col-12">
@@ -11,7 +11,6 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
             <div class="form-group">
                 <label for="name">عنوان</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter name ..." value="<?php echo $category['name']; ?>">
-                <!--            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
             </div>
             <button type="submit" class="btn btn-primary btn-sm">بروز رسانی</button>
         </form>
