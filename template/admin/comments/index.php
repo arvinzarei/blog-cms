a08f3cc0fd81340dfa8279542df09d92aac45ae0
arvin zarei <arvinzarei123@gmail.com>
2021-05-11 20:57:36 +0430

    Update index in comments

--- template/admin/comments/index.php
+++ template/admin/comments/index.php
@@ -1,26 +1,27 @@
 <?php
 require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
 ?>
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h5 "><i class="fas fa-newspaper"></i> نظرات</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            <a role="button" href="#" class="btn btn-sm btn-success disabled">ایجاد</a>
-        </div>
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h5 "><i class="fas fa-newspaper"></i> نظرات</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+        <a role="button" href="#" class="btn btn-sm btn-success disabled">ایجاد</a>
     </div>
-    <section class="table-responsive">
-        <table class="table table-striped table-sm">
-            <caption>لیست نظرات</caption>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>نویسنده</th>
-                    <th>دسته بندی</th>
-                    <th>نظر</th>
-                    <th>وضعیت</th>
-                    <th>تنظیمات</th>
-                </tr>
-            </thead>
-            <tbody>
+</div>
+<section class="table-responsive">
+    <table class="table table-striped table-sm">
+        <caption>لیست نظرات</caption>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>نویسنده</th>
+                <th>دسته بندی</th>
+                <th>نظر</th>
+                <th>وضعیت</th>
+                <th>تنظیمات</th>
+            </tr>
+        </thead>
+        <tbody>
             <?php foreach ($comments as $comment) {?>
                 <tr>
                     <td><a href="http://localhost/admin-panel/comment/show/<?php echo $comment['id']; ?>">1</a></td>
@@ -32,14 +33,15 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
                         <?php if($comment['status']  == 'seen') {?>
                         <a role="button" class="btn btn-sm btn-success text-white" href="http://localhost/admin-panel/comment/approved/<?php echo $comment['id']; ?>">تایید</a>
                         <?php } else{?>
-                          <a role="button" class="btn btn-sm btn-info text-white" href="http://localhost/admin-panel/comment/approved/<?php echo $comment['id']; ?>">عدم تایید</a>
+                        <a role="button" class="btn btn-sm btn-info text-white" href="http://localhost/admin-panel/comment/approved/<?php echo $comment['id']; ?>">عدم تایید</a>
                         <?php } ?>
-                      </td>
+                    </td>
                 </tr>
             <?php  }?>
-            </tbody>
-        </table>
-    </section>
-    <?php
-    require_once(realpath(dirname(__FILE__) . "/../layouts/footer.php"));
-    ?>
+        </tbody>
+    </table>
+</section>
+
+<?php
+require_once(realpath(dirname(__FILE__) . "/../layouts/footer.php"));
+?>
