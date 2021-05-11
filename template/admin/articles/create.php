fe3819058fc9fd8e02636f69b5107800c8e789b0
arvin zarei <arvinzarei123@gmail.com>
2021-05-11 20:50:59 +0430

    Update Create in articles

--- template/admin/articles/create.php
+++ template/admin/articles/create.php
@@ -1,15 +1,12 @@
 <?php
 require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
 ?>
-
-
-        <section class="pt-3 pb-1 mb-2 border-bottom">
+<section class="pt-3 pb-1 mb-2 border-bottom">
     <h1 class="h5">ایجاد پست جدید</h1>
 </section>
 
 <section class="row my-3">
     <section class="col-12">
-
         <form method="post" action="http://localhost/admin-panel/article/store" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="title">عنوان</label>
@@ -40,7 +37,6 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
                 <label for="body">متن پست</label>
                 <textarea class="form-control" id="body" name="body" placeholder="body ..." rows="5" required autofocus></textarea>
             </div>
-
             <button type="submit" class="btn btn-primary btn-sm">ذخیره</button>
         </form>
     </section>
