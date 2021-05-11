7397aeacaaf641f01d7418462e7a8a319464f576
arvin zarei <arvinzarei123@gmail.com>
2021-05-11 21:05:42 +0430

    Update web-setting pages

--- template/admin/web-setting/index.php
+++ template/admin/web-setting/index.php
@@ -2,24 +2,22 @@
 require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
 ?>
 
-
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h5 "><i class="fas fa-newspaper"></i> تنظیمات سایت</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            <a role="button" href="http://localhost/admin-panel/web-setting/set" class="btn btn-sm btn-success">تنظیم کردن</a>
-        </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h5 "><i class="fas fa-newspaper"></i> تنظیمات سایت</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+        <a role="button" href="http://localhost/admin-panel/web-setting/set" class="btn btn-sm btn-success">تنظیم کردن</a>
     </div>
-    <section class="table-responsive">
-        <table class="table table-striped table-sm">
-            <caption>تنظیمات سایت</caption>
-            <thead>
+</div>
+<section class="table-responsive">
+    <table class="table table-striped table-sm">
+        <caption>تنظیمات سایت</caption>
+        <thead>
             <tr>
                 <th>نام</th>
                 <th>مقدار</th>
             </tr>
-            </thead>
-            <tbody>
-
+        </thead>
+        <tbody>
             <tr>
                 <td>عنوان</td>
                 <td><?php echo $setting['title']; ?></td>
@@ -40,10 +38,9 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
                 <td>آیکن</td>
                 <td><img src="<?php echo $setting['icon']; ?>" alt="" width="100px" height="100px"</td>
             </tr>
-            </tbody>
-        </table>
-    </section>
-
+        </tbody>
+    </table>
+</section>
 
 <?php
 require_once(realpath(dirname(__FILE__) . "/../layouts/footer.php"));
